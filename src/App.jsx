c8459e4808809018c7f92e99613fa2ee4028a33c
Quevedo-Diego <quevedodiego593@gmail.com>
2024-05-24 04:31:17 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import TarjetaPresentacion from './components/TarjetaPresentacion';
+import Contador from './components/Contador';
+import GeneradorColores from './components/GeneradorColores';
+import Emojis from './components/Emojis';
+import Adivinanza from './components/Adivinanza';
+import './App.css'; // Importamos el nuevo archivo CSS
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <TarjetaPresentacion 
+        nombre="Diego" 
+        apellido="Quevedo" 
+        profesion="Estudiante" 
+        descripcion="Soy Quevedo Diego estudio la tecnicatura de desarrollo de software" 
+      />
+      <Contador />
+      <GeneradorColores />
+      <Emojis />
+      <Adivinanza />
+    </div>
+  );
+};
 
-export default App
+export default App;
